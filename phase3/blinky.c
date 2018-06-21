c85ba849aa4060d9051c9dd67d9713ef78cf098d
ashnikel <ashnikel@gmail.com>
2018-06-21 17:56:13 +0300

    Blinking in C

--- phase3/blinky.c
+++ phase3/blinky.c
@@ -16,5 +16,14 @@ static void spin_sleep_ms(unsigned int ms) {
 
 int main(void) {
   // STEP 1: Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 |= 0b001 << 18;
+
   // STEP 2: Continuously set and clear GPIO 16.
+  while (1) {
+    *GPIO_SET0 |= 1 << 16;
+    spin_sleep_ms(300);
+
+    *GPIO_CLR0 |= 1 << 16;
+    spin_sleep_ms(700);
+  }
 }
