6aaad3cbd6259202b1cbc4794a71572507e15b0b
ashnikel <ashnikel@gmail.com>
2018-06-21 18:23:13 +0300

    Blinking in Rust

--- phase4/src/lib.rs
+++ phase4/src/lib.rs
@@ -22,5 +22,12 @@ fn spin_sleep_ms(ms: usize) {
 #[no_mangle]
 pub unsafe extern "C" fn kmain() {
     // STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() | 0b001 << 18);
     // STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(GPIO_SET0.read_volatile() | 1 << 16);
+        spin_sleep_ms(10000);
+        GPIO_CLR0.write_volatile(GPIO_CLR0.read_volatile() | 1 << 16);
+        spin_sleep_ms(10000);
+    }
 }
